82e6541c817317e119d9b1e3553b943ebce1b994
ademi <ademi@Vesubio10>
2020-03-22 21:01:23 -0400

    Im done..!

--- Lab3/src/Exercises/isPalindrome.java
+++ Lab3/src/Exercises/isPalindrome.java
@@ -6,58 +6,58 @@ import stacks.LinkedListStack;
 
 public class isPalindrome  {
 
+	public static void main(String[] args) {
+		scan();
+	}
+	public static void scan() {
 
-		public static void scan() {
-			
-			String s;
-			
-			System.out.println("Input value: (Press Enter twice to stop program):");
-			
-			@SuppressWarnings("resource")
-			Scanner sc = new Scanner(System.in);
-			s = sc.nextLine();
-			
-			if(s.equals("")) {
-				System.exit(0);
-			}
-			
-			printConfirmedPalindrome(s);
-			scan();
-		}
-		
-		public static void printConfirmedPalindrome(String s) {
-			if(isPalindrome(s)) {
-				System.out.println("Palindrome");
-			}else {
-				System.out.println("Not a Palindrome");
-			}
-		}
-		
-		public static boolean isPalindrome(String s) {
-			s = s.toLowerCase().replaceAll("[^a-zA-Z]","");
+		String s;
 
-			DoublyLinkedQueue<Character> queue = new DoublyLinkedQueue<>();
-			LinkedListStack<Character> stack = new LinkedListStack<>();
+		System.out.println("Input value: (Press Enter twice to stop program):");
 
-			for(int i = 0; i < s.length(); i++) {
-				queue.enqueue(s.charAt(i));
-				stack.push(s.charAt(i));
-			}
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		s = sc.nextLine();
 
-			return isPalindrome(queue, stack);
+		if(s.equals("")) {
+			System.exit(0);
 		}
 
-		public static boolean isPalindrome(DoublyLinkedQueue<Character> q , LinkedListStack<Character> s) {
+		printPalindrome(s);
+		scan();
+	}
+	public static boolean isValidPalindrome(DoublyLinkedQueue<Character> q , LinkedListStack<Character> s) {
 
-			if(q.size() == 0 && s.size() == 0) {
-				return true;
-			}else if(q.dequeue().equals(s.pop())) {
-				return isPalindrome(q, s);
-			}
-			return false;
+		if(q.size() == 0 && s.size() == 0) {
+			return true;
+		}else if(q.dequeue().equals(s.pop())) {
+			return isValidPalindrome(q, s);
+		}
+		return false;
+	}
+
+
+
+
+	public static boolean adjustingPalindrome(String s) {
+		s = s.toLowerCase().replaceAll("[^a-zA-Z]","");
+
+		DoublyLinkedQueue<Character> queue = new DoublyLinkedQueue<>();
+		LinkedListStack<Character> stack = new LinkedListStack<>();
+
+		for(int i = 0; i < s.length(); i++) {
+			queue.enqueue(s.charAt(i));
+			stack.push(s.charAt(i));
 		}
 
-		public static void main(String[] args) {
-			scan();
+		return isValidPalindrome(queue, stack);
+	}
+
+	public static void printPalindrome(String s) {
+		if(adjustingPalindrome(s)) {
+			System.out.println("Palindrome");
+		}else {
+			System.out.println("Not a Palindrome");
 		}
 	}
+}
